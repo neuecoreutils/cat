913b824584a7921ea442c6365cbfbe5606c555a5
MP Prabhanand <Mwdxp@rediffmail.com>
2022-06-08 20:06:05 +0530

    line number support added!

--- Sources/cat/cat.swift
+++ Sources/cat/cat.swift
@@ -2,31 +2,61 @@
 import ArgumentParser
 import Foundation
 
+func concat(_ f: String) {
+    if FileManager.default.fileExists(atPath: f) {
+        let file = FileHandle(forReadingAtPath: f)
+        if file != nil {
+            let data = file!.readDataToEndOfFile()
+            if data.count > 0 {
+                let str = NSString(data: data, encoding: String.Encoding.utf8.rawValue)
+                if str != nil {
+                    print(str!)
+                }
+            }
+        }
+    }
+
+    else {
+        print("File does not exist.")
+    }
+}
+
+func catWithLineNums(_ f: String) {
+    if FileManager.default.fileExists(atPath: f) {
+
+        let file = FileHandle(forReadingAtPath: f)
+        if file != nil {
+            let data = file!.readDataToEndOfFile()
+            if data.count > 0 {
+                let str = NSString(data: data, encoding: String.Encoding.utf8.rawValue)
+                if str != nil {
+                    let lines = str!.components(separatedBy: "\n")
+                    for (index, line) in lines.enumerated() {
+                        print("    \(index + 1)    \(line)")
+                    }
+                }
+            }
+        }  
+    }
+
+    else {
+        print("File does not exist.")
+    }
+}
+
 @main
 struct cat: ParsableCommand {
     @Flag(help: "Print the line numbers along with each line.")
-    var lineNumbers: Bool = false
+    var number: Bool = false
 
     @Argument(help: "Path of the file to concatenate.")
     var fileName: String
 
     mutating func run() throws {
-        if FileManager.default.fileExists(atPath: fileName) {
-
-            let file = FileHandle(forReadingAtPath: fileName)
-            if file != nil {
-                let data = file!.readDataToEndOfFile()
-                if data.count > 0 {
-                    let str = NSString(data: data, encoding: String.Encoding.utf8.rawValue)
-                    if str != nil {
-                        print(str!)
-                    }
-                }
-            }
-        }
-
-        else {
-            print("File does not exist.")
+        if number {
+            catWithLineNums(fileName)
+        } else {
+            concat(fileName)
         }
     }
 }
